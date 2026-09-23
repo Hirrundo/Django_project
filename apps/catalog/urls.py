ae8ec224ca857cdba3b7feed666ebf0400788911
Liza <lizzkalizzka@inbox.ru>
2026-09-23 20:29:14 +0300

    шаблоны

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.urls import path
 from .views import get_catalog,get_by_id
 
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Dish
 
 
 def my_view(request):
@@ -8,10 +10,24 @@ def my_view(request):
 def get_by_id(request,id):
     return JsonResponse({'data':id })
 
+def get_by_id(request,id):
+    dish=Dish.objects.filter(pk=id).first()
+    # dish=get_object_or_404(Dish,pk=id)
+    return render(request,'catalog/dijdeteil.html',
+                  {
+        'dish':dish,
+        
+        })
+
 def  hello (request,name:str):
     return HttpResponse(f'<h1> Hello,{name}</h1>')
 
 def get_catalog(request):
+    dishes=Dish.objects.all()
+    content={
+        'dishes':dishes
+    }
     return render(request,'catalog/catlist.html')
+
 
 # Create your views here.
